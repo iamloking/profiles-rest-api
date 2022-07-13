ae1e13b8984714f63d12aab1b95edc95943449c3
Lokesh <hopelokesh@gmail.com>
2022-07-13 09:04:23 +0530

    Added HelloApiView

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -13,11 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from profiles_api import views
+from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('profiles_api.urls')),
-
+    path('hello-view/', views.HelloApiView.as_view()),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+    def get(self,request,format=None):
+        """Returns a list of APIviews features"""
+        an_apiview = [
+                    'Uses HTTP methods as function (get,post,put,patch,delete)',
+                    "Gives you the most control over your app logic",
+                    "Is mapped manually to URLS"
+        ]
+
+        return Response({'message':'Hello','an_apiview':an_apiview})
