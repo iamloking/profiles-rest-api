a1d1787ae5286f38cb740fce32ec2f4a825c1efe
Lokesh <hopelokesh@gmail.com>
2022-07-13 20:18:00 +0530

    Added addition HTTP functionality like Put, Patch, Delete

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializer_class = serializers.HelloSerializer
     def get(self,request,format=None):
         """Returns a list of APIviews features"""
         an_apiview = [
@@ -13,3 +15,27 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello','an_apiview':an_apiview})
+
+    def post(self,request):
+        """Create a hello message with user's name"""
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f"Hello {name}"
+            return Response({'message':message})
+        else:
+            return Response(
+                    serializer.errors,
+                    status = status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request,pk=None):
+        """Handle updating an object"""
+        return Response({'method':'put'})
+
+    def patch(self,request,pk=None):
+        """Handle partial updating of an object"""
+        return Response({'method':'patch'})
+
+    def delete(self,request,pk=None):
+        """Delete an object"""
+        return Response({'method':'delete'})
