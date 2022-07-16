80a2f322c92695441d12d5debfefdc42de12297e
Lokesh <hopelokesh@gmail.com>
2022-07-16 14:42:23 +0530

    Added login authentication

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -20,5 +20,6 @@ router = DefaultRouter()
 router.register('profile',views.UserProfileViewSet)
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/',views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
